f6998096a716b68eeb88aa921698562647325646
davidserranogemes <davidserranogemes@hotmail.com>
2020-02-09 21:12:36 +0100

    Testing break pot function generator

--- __main__.py
+++ __main__.py
@@ -19,10 +19,25 @@ n_changes = 10000
 
 X,Y,fitness = bm.generate_border(radius,n_dots,n_iter,gain, n_changes, False)
 
-plt.plot(range(len(fitness)),fitness, label = 'linear')
+#plt.plot(range(len(fitness)),fitness, label = 'linear')
 
-plt.show()
+#plt.show()
 
 plt.plot(X,Y, label='linear')
+
+#plt.show()
+
+
+Xi,Yi, fitness_i = bm.generate_border(radius/2,n_dots-50,n_iter, gain/2, n_changes,False)
+
+plt.plot(Xi,Yi, label='linear')
+
+
+
+br_point_ex1,br_point_ex2, br_point_in1,br_point_in2 = bm.segment_get_break_points(X,Y,Xi,Yi,round(n_dots/4),round(n_dots/10))
+
+
+plt.plot([X[br_point_ex1],Xi[br_point_in1]],[Y[br_point_ex1],Yi[br_point_in1]], label='linear')
+plt.plot([X[br_point_ex2],Xi[br_point_in2]],[Y[br_point_ex2],Yi[br_point_in2]], label='linear')
 
 plt.show()
